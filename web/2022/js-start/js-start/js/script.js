ba428437e2266628868b4c34af7e4a023be5edc5
Ilyaag <dindo1995@gmail.com>
2022-10-22 19:39:43 +0400

    js_hw

--- web/2022/js-start/js-start/js/script.js
+++ web/2022/js-start/js-start/js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+/*
+//Задача 5
+const myName = 'Илья'
+const mySurname = 'Агеев'
+const myWork = 'барбер'
+
+const personInfo2 = `Меня зовут так ${myName} ${mySurname} и я ${myWork} и это первая строка
+а это вторая строка и следующая будет ниже
+третья вот`
+console.log(personInfo2) */
+// 6 задача Типы данных
+const strigSt = 'TestString'
+const numNum = 10
+const booleanNum = true
+const nullNum = null
+let unNum
+const object = {}
+const arrayNum = []
+
+console.log(typeof strigSt)
+console.log(typeof numNum)
+console.log(typeof booleanNum)
+console.log(typeof nullNum)
+console.log(typeof unNum)
+console.log(typeof object)
+console.log(typeof arrayNum)
