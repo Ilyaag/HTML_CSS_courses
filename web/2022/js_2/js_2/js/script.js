f98c744b5b2fd0b99ceb21bb39ecdf1aee7a199f
Ilyaag <dindo1995@gmail.com>
2022-10-30 20:44:38 +0400

    js_2

---
+++ web/2022/js_2/js_2/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+if (4 == 9) {
+    console.log('okay');
+} else {
+    console.log('error');
+}
+
+const num = 101
+
+if (num < 49) {
+    console.log('ERROR');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('ok!');
+}
+
+(num === 50) ? console.log('ok!') : 
