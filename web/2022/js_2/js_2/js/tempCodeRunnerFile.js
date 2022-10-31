b9e52198372de98df896feea172ac91d6336a332
Ilyaag <dindo1995@gmail.com>
2022-10-31 20:01:32 +0400

    js_2

--- web/2022/js_2/js_2/js/tempCodeRunnerFile.js
+++ web/2022/js_2/js_2/js/tempCodeRunnerFile.js
@@ -1,4 +1,7 @@
-while (num <= 55) { //первый вариант цикла. тикает счетчик пока число не станет <=55
-//     console.log(num);
-//     num++;
-// }
+for ( let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a]=b;
+};
+
+console.log(personalMovieDB);
