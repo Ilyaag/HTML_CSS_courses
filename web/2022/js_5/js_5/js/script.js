89e1a5f447522ef7539c5478a8d1b853cd122308
Ilyaag <dindo1995@gmail.com>
2022-11-27 16:18:33 +0400

    Js_5

--- web/2022/js_5/js_5/js/script.js
+++ web/2022/js_5/js_5/js/script.js
@@ -1,3 +1,34 @@
 'use strict';
 
-const box = document.getElementById();
+const box = document.getElementById('box');//устаревшее
+console.log(box);
+
+//выведет сразу всю коллекцию
+//const btns = document.getElementsByTagName('button');
+//console.log(btns);
+
+//получаем отдельную кнопку
+//const btns = document.getElementsByTagName('button')[1];
+//console.log(btns);
+
+//2-й способ
+//const btns = document.getElementsByTagName('button');
+//console.log(btns[1]);
+
+
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+//-----БОЛЕЕ СОВРЕМЕННЫЕ МЕТОДЫ НИЖЕ---------
+
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+
+//c помощью этого метода мы вывели все 3 сердечка в консоль
+hearts.forEach(item => {
+   console.log(item);
+});
+
+//метод позволяет получить 1-й элемент со страницы
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
