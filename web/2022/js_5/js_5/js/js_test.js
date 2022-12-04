e4d72127bc4b7024f5439f20c05fc3885bedb026
Ilyaag <dindo1995@gmail.com>
2022-12-04 22:09:39 +0400

    js_7

--- web/2022/js_5/js_5/js/js_test.js
+++ web/2022/js_5/js_5/js/js_test.js
@@ -1,11 +1,12 @@
 'use strict';
 
-//const box = document.getElementById('box');//устаревшее
-//console.log(box);
+// const box = document.getElementById('box');//устаревшее
+// console.log(box);
 
 //выведет сразу всю коллекцию
-//const btns = document.getElementsByTagName('button');
-//console.log(btns);
+/*Метод определён для объекта document и любого HTML-элемента (Element) страницы. Позволяет найти все элементы с заданным тегом среди дочерних. Возвращает похожую на массив HTMLCollection с найденными элементами. Если элементов не нашлось, то коллекция будет пустая, то есть с размером 0.*/
+// const btns = document.getElementsByTagName('button');
+// console.log(btns);
 
 //получаем отдельную кнопку
 //const btns = document.getElementsByTagName('button')[1];
@@ -36,79 +37,79 @@
 
 //-----------------------------------------//
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart'),
-      wrapper = document.querySelector('.wrapper');
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
+// const box = document.getElementById('box'),
+//       btns = document.getElementsByTagName('button'),
+//       circles = document.getElementsByClassName('circle'),
+//       hearts = document.querySelectorAll('.heart'),
+//       oneHeart = document.querySelector('.heart'),
+//       wrapper = document.querySelector('.wrapper');
+// // box.style.backgroundColor = 'blue';
+// // box.style.width = '500px';
 
-//сделать 2-ю кнопку овальной
-btns[1].style.borderRadius = '100%';
-//cделать 1-й круг красным
-circles[0].style.backgroundColor = 'red';
+// //сделать 2-ю кнопку овальной
+// btns[1].style.borderRadius = '100%';
+// //cделать 1-й круг красным
+// circles[0].style.backgroundColor = 'red';
 
-//назначаем несколько инлайн стилей
-box.style.cssText = `background-color: blue; width: 500px`;
-
-
-//если нужно провести действия над несколькими элементами
-
-//цикл(редко)
-// for (let i = 0; i < hearts.length; i++) {
-//    hearts[i].style.backgroundColor = 'blue';
-// }
-
-//перебирающий метод(часто)
-hearts.forEach(item => {
-   item.style.backgroundColor = 'blue';
-});
+// //назначаем несколько инлайн стилей
+// box.style.cssText = `background-color: blue; width: 500px`;
 
 
-//-- ОСНОВНЫЕ МЕТОДЫ ДЛЯ РАБОТЫ С ЭЛЕМЕНТАМИ СТРАНИЦЫ --
+// //если нужно провести действия над несколькими элементами
 
-const div = document.createElement('div');
-//const text = document.createTextNode('Тут был я');
+// //цикл(редко)
+// // for (let i = 0; i < hearts.length; i++) {
+// //    hearts[i].style.backgroundColor = 'blue';
+// // }
 
-//визуальная оболочка для элемента
-div.classList.add('black');
-
-//-- cовременные методы
-
-//добавляем див в конец родительского тега бади
-document.body.append(div);
-
-//вариант 2
-//wrapper.append(div);
-
-//обратный append (в конец элемент) есть метод который ставит в начало PREPEND
-// wrapper.prepend(div);
-
-//перед первым сердечком поставил черный квадрат
-//hearts[0].before(div);
-
-//после первого сердечка черный квадрат
-//hearts[0].after(div)
-
-//УДАЛЕНИЕ ЭЛЕМЕНТА СО СТР
-//circles[0].remove();
-
-//ОДИН ЭЛЕМЕНТ ЗАМЕНИТЬ ДРУГИМ
-//hearts[0].replaceWith(circles[0]);
+// //перебирающий метод(часто)
+// hearts.forEach(item => {
+//    item.style.backgroundColor = 'blue';
+// });
 
 
-//-- РЕДАКТИРОВАНИИЕ ПРЯМО В ЭЛЕМЕНТЫ
+// //-- ОСНОВНЫЕ МЕТОДЫ ДЛЯ РАБОТЫ С ЭЛЕМЕНТАМИ СТРАНИЦЫ --
 
-//вписать простой текст в элемент
-//работа с html структурой
-div.innerHTML = '<h1>Hello World</h1>';//можем еще вставлять HTML структуру
+// const div = document.createElement('div');
+// //const text = document.createTextNode('Тут был я');
 
-//только текст от пользователя
-//div.textContent = 'Hello'; //метод только с текстом
+// //визуальная оболочка для элемента
+// div.classList.add('black');
+
+// //-- cовременные методы
+
+// //добавляем див в конец родительского тега бади
+// document.body.append(div);
+
+// //вариант 2
+// //wrapper.append(div);
+
+// //обратный append (в конец элемент) есть метод который ставит в начало PREPEND
+// // wrapper.prepend(div);
+
+// //перед первым сердечком поставил черный квадрат
+// //hearts[0].before(div);
+
+// //после первого сердечка черный квадрат
+// //hearts[0].after(div)
+
+// //УДАЛЕНИЕ ЭЛЕМЕНТА СО СТР
+// //circles[0].remove();
+
+// //ОДИН ЭЛЕМЕНТ ЗАМЕНИТЬ ДРУГИМ
+// //hearts[0].replaceWith(circles[0]);
 
 
-//-----работа с частями кода HTML - insertAdjacentHTML
-//вставить данные HTML перед элементом
-//div.insertAdjacentHTML('beforebegin','<h2>Hello</h2>');
+// //-- РЕДАКТИРОВАНИИЕ ПРЯМО В ЭЛЕМЕНТЫ
+
+// //вписать простой текст в элемент
+// //работа с html структурой
+// div.innerHTML = '<h1>Hello World</h1>';//можем еще вставлять HTML структуру
+
+// //только текст от пользователя
+// //div.textContent = 'Hello'; //метод только с текстом
+
+
+// //-----работа с частями кода HTML - insertAdjacentHTML
+// //вставить данные HTML перед элементом
+// //div.insertAdjacentHTML('beforebegin','<h2>Hello</h2>');
--- web/2022/js_7 practice_2/project/js/script.js
+++ web/2022/js_7 practice_2/project/js/script.js
@@ -33,31 +33,65 @@ const adv = document.querySelectorAll('.promo__adv img'),
       genre = poster.querySelector('.promo__genre'),
       movieList = document.querySelector('.promo__interactive-list'),
       addForm = document.querySelector('form.add'),
-      addImput = document.querySelector('.adding__input');
-      //checkbox = document.querySelector('найти через атрибуты');
-
-//console.log(addForm);
+      addImput = addForm.querySelector('.adding__input'),
+      checkbox = document.querySelector('[type="checkbox"]');
 
 
+/*назначаем обработчик события на форму submit
+сначала отменяем стандартное поведение браузера
+что именно пользователь ввел и поставил ли галочку
+cначала обратились к импуту с которым взаимодействовал пользователь, в велюе будет содержаться то, что ввел пользователь
+далее обращаемся к чекбоксу переменная фейворит. Это свойство checked можем получить в ДЖС чтобы получить булиновое значение
+далее обращаемся к фильмам, фильмы это массив и тот фильм который был введен пользователем - пушится.
+Когда мы запушили новый фильм - сортируем по алфавиту 
+Далее берем пример как мы создавали массив ниже, и на его основании делаем то,что сначала надо удалить список фильмов, занести туда данные и снова заполнить его
+Далее оптимизируем функцию на строке 74*/
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const newFilm = addImput.value;
+    const favorite = checkbox.checked;
+
+    movieDB.movies.push(newFilm);
+    movieDB.movies.sort();
+})
 
 
 
-adv.forEach(item => {
-    item.remove();
-});
 
-genre.textContent = 'Драма';
 
-poster.style.backgroundImage = `url('img/bg.jpg')`;
+const deliteAdv = (arr) => {
+    arr.forEach(item => {
+        item.remove();
+    });
+};
 
-movieList.innerHTML = '';
+deliteAdv(adv);
+
+
+
+const makeChanges = () => {
+    genre.textContent = 'Драма';
+
+    poster.style.backgroundImage = `url('img/bg.jpg')`;
+};
+
+makeChanges();
 
 movieDB.movies.sort();
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
+    function createMovieList(films, parent ) {
+        parent.innerHTML = '';
+
+
+        
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+    }
+//когда первый раз заходим на страницу, отображаем на странице и перебираем агрументы
+createMovieList(movieDB.movies, movieList);
